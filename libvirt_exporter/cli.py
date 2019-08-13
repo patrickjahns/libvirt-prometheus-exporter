e354e529330f962ece2313bb1fcda00a4fb8ded3
Patrick Jahns <github@patrickjahns.de>
2019-08-14 01:32:11 +0200

    added cli/env vars in order to configure exporter
    Signed-off-by: Patrick Jahns <github@patrickjahns.de>

--- libvirt_exporter/cli.py
+++ libvirt_exporter/cli.py
@@ -1,2 +1,24 @@
+import time
+import configargparse
+from .collector import LibvirtCollector
+from prometheus_client.core import REGISTRY
+from prometheus_client import start_http_server
+
+
 def main():
-    pass
+    p = configargparse.ArgParser(add_config_file_help=False,
+                                 auto_env_var_prefix='LIBVIRT_EXPORTER_'
+                                 )
+    p.add('--uri', required=True,
+          help='libvirt connection string (i.e. qemu:///system)')
+    p.add('--port',
+          help='port where the exporter will listen (default: 9233)',
+          default=9233)
+    p.add('--host',
+          help='address where the exporter will listen (default: 0.0.0.0 )',
+          default='0.0.0.0')
+    options = p.parse_args()
+    REGISTRY.register(LibvirtCollector(options.uri))
+    start_http_server(options.port, addr=options.host)
+    while (True):
+        time.sleep(10000)
